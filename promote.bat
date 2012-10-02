e155048ebf73c271376026002d1b79ad82bbef65
Vitalii Biliienko <vitalii.biliienko@gmail.com>
2012-10-02 14:40:02 +0300

    wedq

--- promote.bat
+++ promote.bat
@@ -1,4 +1,6 @@
-echo %1
-%1 add %2
-%1 commit -m "Version update"
-%1 push origin master
+C:\Users\vitalii.biliienko\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\bin\git.exe add Version.cs
+echo ERRORLEVEL
+C:\Users\vitalii.biliienko\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\bin\git.exe commit -m "Version update"
+echo ERRORLEVEL
+C:\Users\vitalii.biliienko\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\bin\git.exe push origin master
+echo ERRORLEVEL
