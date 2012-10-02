56145df653076606d37c3067206950213d998808
unknown <vitalii.biliienko@KBP1-DHP-F47477.synapse.com>
2012-10-02 15:31:15 +0300

    Version update

--- Version.cs
+++ Version.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.62.0")]
 [assembly: AssemblyFileVersion("1.0.62.0")]
-[assembly: AssemblyInformationalVersion("1.0.62.64a3b5821e9a903ac5036a190300804bfdeb8c43")]
+[assembly: AssemblyInformationalVersion("1.0.62.99a3b5821e9a903ac5036a190300804bfdeb8c43")]
