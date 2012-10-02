ff673b0bc0f9a336a29cd80b8a4adb1fba55307c
Vitalii Biliienko <vitalii.biliienko@gmail.com>
2012-10-02 15:58:53 +0300

    asda

--- promote.bat
+++ promote.bat
@@ -1,7 +1,9 @@
 set PATH=PATH;C:\Users\vitalii.biliienko\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\bin;
 echo %USERDOMAIN%\%USERNAME%
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
 copy Version.cs D:\git\TestRepo >nul
 cd D:\git\TestRepo
-call git.cmd add Version.cs
-call git.cmd commit -m "Version update"
-call git.cmd push origin master
+call git.exe add Version.cs
+call git.exe commit -m "Version update"
+call git.exe push origin master
