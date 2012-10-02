fa7c48eaf76cea1760d43259708db3d2be6b798f
unknown <vitalii.biliienko@KBP1-DHP-F47477.synapse.com>
2012-10-02 15:35:24 +0300

    Version update

--- Version.cs
+++ Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.62.0")]
-[assembly: AssemblyFileVersion("1.0.62.0")]
-[assembly: AssemblyInformationalVersion("1.0.62.99a3b5821e9a903ac5036a190300804bfdeb8c43")]
+[assembly: AssemblyVersion("1.0.63.0")]
+[assembly: AssemblyFileVersion("1.0.63.0")]
+[assembly: AssemblyInformationalVersion("1.0.63.3b68127529ae3c43e0c72bdfd867839ddedb5e45")]
